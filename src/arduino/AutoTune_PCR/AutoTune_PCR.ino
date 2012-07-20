6fe76f2a604378151f04497ffe6f281ca8be97db
Scott Howard <showard314@gmail.com>
2012-07-20 16:53:38 -0400

    updated autotune file

--- src/arduino/AutoTune_PCR/AutoTune_PCR.ino
+++ src/arduino/AutoTune_PCR/AutoTune_PCR.ino
@@ -1,15 +1,21 @@
+//program to autotune the pcr. Set the "setpoint" variable
+//and read the optimized PID from the serial port
+//use "SetControlType" to 0 for PI or 1 for PID (in changeAutoTune)
+
 #include <PID_v1.h>
 #include <PID_AutoTune_v0.h>
 #include <math.h>
 
 byte ATuneModeRemember=2;
-double input=25, output=255, setpoint=80;
+double input=25, output=255, setpoint=90;
 //double kp=5.11,ki=.5,kd=0; //at 50 C
-double kp=6.25,ki=.15,kd=0; //at 100 C
+//double kp=6.25,ki=.15,kd=0; //at 100 C
+//double kp=22.19,ki=.4,kd=0; //at 50 C new box
+double kp=26.49,ki=.68,kd=0; //at 50 C new box and fan
 
 double kpmodel=1.5, taup=100, theta[50];
 double outputStart=5;
-double aTuneStep=50, aTuneNoise=1, aTuneStartValue=100;
+double aTuneStep=127, aTuneNoise=1, aTuneStartValue=128;
 unsigned int aTuneLookBack=20;
 
 boolean tuning = true;
@@ -18,13 +24,22 @@ unsigned long  modelTime, serialTime;
 PID myPID(&input, &output, &setpoint,kp,ki,kd, DIRECT);
 PID_ATune aTune(&input, &output);
 
+
 //set to false to connect to the real world
 boolean useSimulation = false;
 
+const int heater=3;
+const int fan = 5;
+
 void setup()
 {
-  pinMode(2, OUTPUT);
-  digitalWrite(2,HIGH);
+  double sensorValue1;
+  double r1;
+  double temp;
+  pinMode(heater, OUTPUT);
+  pinMode(fan, OUTPUT);
+  //digitalWrite(2,HIGH);
+  aTune.SetControlType(1);//set for PID extraction instead of PI
   if(useSimulation)
   {
     for(byte i=0;i<50;i++)
@@ -46,6 +61,21 @@ void setup()
   serialTime = 0;
   Serial.begin(9600);
 
+  if(!useSimulation)
+  { //pull the input in from the real world
+    sensorValue1 = analogRead(0);
+    r1 = 1000.0/((1023.0/sensorValue1)-1);
+    temp = 3560.0/log(r1/0.0130444106) - 273.15;
+    digitalWrite(heater,HIGH);
+    while(temp<setpoint){
+      sensorValue1 = analogRead(0);
+      r1 = 1000.0/((1023.0/sensorValue1)-1);
+      temp = 3560.0/log(r1/0.0130444106) - 273.15;
+      Serial.print(temp);Serial.println();
+    }
+    digitalWrite(heater,LOW);
+    
+  }
 }
 
 void loop()
@@ -58,7 +88,7 @@ void loop()
   if(!useSimulation)
   { //pull the input in from the real world
     sensorValue1 = analogRead(0);
-    r1 = 2000.0*(1024.0/sensorValue1) - 2000.0;
+    r1 = 1000.0/((1023.0/sensorValue1)-1);
     input = 3560.0/log(r1/0.0130444106) - 273.15;
   }
   
@@ -91,7 +121,20 @@ void loop()
   }
   else
   {
-     analogWrite(3,output); 
+//     if(output>=128){
+//       digitalWrite(fan,LOW);
+//       analogWrite(heater,(output-128)*2);
+//       //Serial.print("heater ");Serial.print((output-128)*2);Serial.println();
+//     }
+//     if(output<=127){
+//       digitalWrite(heater,LOW);
+//       analogWrite(fan,(127-output)*2);
+//       //Serial.print("fan ");Serial.print((127-output)*2);Serial.println();
+//     }
+     analogWrite(heater,output);
+     if(input>setpoint+1){
+       digitalWrite(fan,HIGH);
+     } else digitalWrite(fan,LOW);
   }
   
   //send-receive with processing if it's time
@@ -111,6 +154,7 @@ void changeAutoTune()
     output=aTuneStartValue;
     aTune.SetNoiseBand(aTuneNoise);
     aTune.SetOutputStep(aTuneStep);
+    aTune.SetControlType(1);//set for PID extraction instead of PI
     aTune.SetLookbackSec((int)aTuneLookBack);
     AutoTuneHelper(true);
     tuning = true;
